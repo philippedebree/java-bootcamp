7dc14a8fe7463d308814dd92fdcafb7e942762d3
Philippe de Bree <116602470+philippedebree@users.noreply.github.com>
2026-09-02 14:28:19 -0400

    Module 3 Exercise 5

---
+++ examples/module-03-exercises/InterfaceDemo.java
@@ -0,0 +1,6 @@
+public class InterfaceDemo {
+    public static void main(String[] args) {
+        Printable printable = new Customer("C101", "Aman Singh");
+        printable.printDetails();
+    }
+}
